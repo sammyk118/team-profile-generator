41673e0dfc50d293d6d9b6c42945b6130667abc4
Sammy Kroner <sammyk118@gmail.com>
2021-01-24 00:42:29 -0800

    write to html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // TODO: Write code to define and export the Employee class
 const inquirer = require("inquirer");
+const fs = require("fs");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -125,34 +126,44 @@ function addUser() {
             promptUser();
         }
         else
-            console.log("render the html");
+            generateHTML();
     })
 }
-const generateHTML = (data) =>
-    `<!DOCTYPE html>
+function generateHTML() {
+    
+    const html = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+  <title>Team Profile</title>
 </head>
 <body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${data.name}</h1>
-    <p class="lead">I am from ${data.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${data.github}</li>
-      <li class="list-group-item">LinkedIn: ${data.linkedin}</li>
-    </ul>
-  </div>
-</div>
+    <nav class="navbar navbar-dark">
+        <span class ="navbar-brand h1 text-center">Team Profile</span>
+    </nav>
+    <div class="container">
+        <h1 class="display-4">Hi! My name is </h1>
+        <p class="lead">I am from .</p>
+        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+        <ul class="list-group">
+            <li class="list-group-item">My GitHub username is</li>
+            <li class="list-group-item">LinkedIn:</li>
+        </ul>
+    </div>
 </body>
 </html>`;
 
-promptUser()
-//   .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-//   .then(() => console.log('Successfully wrote to index.html'))
-//   .catch((err) => console.error(err));
+    
+    
+    
+    fs.writeFile("./team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
+    
+}
+
+promptUser();
